6b2eca1d10b8920bfc6ab1636717b73badd7b655
A1K32F <44324860+A1K32F@users.noreply.github.com>
2020-12-28 14:30:10 +0100

    Final upload 2020.12.28.

---
+++ SignInDB/LoginDB.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[LoginId] INT NOT NULL , 
+    [username] VARCHAR(50) NULL, 
+    [password] VARCHAR(50) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([LoginId])
+)
---
+++ SignInDB/dbo.Table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Toolsdb]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[Felhasznalo] VARCHAR(50) NULL,
+    [Date] VARCHAR(50) NULL, 
+    [Marka] VARCHAR(50) NULL, 
+    [Tipus] VARCHAR(50) NULL, 
+    [Tartozek] VARCHAR(200) NULL, 
+    [Hordozhato] BIT NULL, 
+    [Javit] BIT NULL, 
+    [Megjegyzes] VARCHAR(300) NULL, 
+    [Vezeteknev] VARCHAR(50) NULL, 
+    [Keresztnev] VARCHAR(50) NULL, 
+    [Telefon] INT NULL, 
+    [Email] VARCHAR(50) NULL
+)
