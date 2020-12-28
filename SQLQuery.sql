62db1d7874564f90b65b6497a06aaae4d392d91f
A1K32F <44324860+A1K32F@users.noreply.github.com>
2020-12-28 14:31:30 +0100

    Query 2020.12.28.

---
+++ SQLQuery.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROC ToolsAdd
+
+
+
+    @Felhasznalo VARCHAR (50) ,
+    @Date        VARCHAR (50)  ,
+    @Marka      VARCHAR (50) ,
+    @Tipus       VARCHAR (50) ,
+    @Tartozek    VARCHAR (200) ,
+    @Hordozhato  BIT           ,
+    @Javit       BIT           ,
+    @Megjegyzes  VARCHAR (300) ,
+    @Vezeteknev  VARCHAR (50)  ,
+    @Keresztnev VARCHAR (50)  ,
+    @Telefon     INT           ,
+    @Email       VARCHAR (50) 
+    
+
+AS
+	INSERT INTO Toolsdb(Felhasznalo,Date,Marka,Tipus,Tartozek,Hordozhato,Javit,Megjegyzes,Vezeteknev,Keresztnev,Telefon,Email)
+	Values (@Felhasznalo,@Date,@Marka,@Tipus,@Tartozek,@Hordozhato,@Javit,@Megjegyzes,@Vezeteknev,@Keresztnev,@Telefon,@Email)
